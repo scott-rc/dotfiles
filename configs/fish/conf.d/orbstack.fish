8aa08d371ec35d714de0bf81172ae0c651abc0c2
Scott Côté <scott.cote@hey.com>
2026-01-20 10:55:34 -0500

    WIP - Tue, Jan 20 10:55 AM

--- configs/fish/conf.d/orbstack.fish
+++ configs/fish/conf.d/orbstack.fish
@@ -3,3 +3,7 @@ if not status is-interactive
 end
 
 brew_ensure orb orbstack
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
