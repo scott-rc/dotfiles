f21b4c3d5ab0d7517b9bb7c3bf98e257caabc904
Scott Côté <scott.cote@hey.com>
2026-02-01 21:32:19 -0500

    Configure fzf to use fd and respect .gitignore
    fzf uses `find` by default which doesn't understand .gitignore. This
    configures `FZF_DEFAULT_COMMAND`, `FZF_CTRL_T_COMMAND`, and
    `FZF_ALT_C_COMMAND` to use `fd` instead, which respects .gitignore by
    default. The `.git` directory is explicitly excluded since it's not
    covered by .gitignore.

--- configs/fish/conf.d/fzf.fish
+++ configs/fish/conf.d/fzf.fish
@@ -4,6 +4,10 @@ end
 
 brew_ensure fzf
 
+set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --follow --exclude .git'
+
 function fzf_prompt --argument-names PROMPT QUERY
     if test -z "$PROMPT"
         set PROMPT ""
