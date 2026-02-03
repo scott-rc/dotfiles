c014a9d07d6f29919310fe0fd1f8a1d228c50ce5
Scott Côté <scott.cote@hey.com>
2026-02-03 14:21:33 -0500

    Fix string join parsing --exclude as option in fzf config

--- configs/fish/conf.d/fzf.fish
+++ configs/fish/conf.d/fzf.fish
@@ -7,7 +7,7 @@ set -l fd_excludes \
     .DS_Store \
     .direnv
 
-set -l fd_exclude_args (string join ' ' (for e in $fd_excludes; echo "--exclude $e"; end))
+set -l fd_exclude_args (string join ' ' -- (for e in $fd_excludes; echo "--exclude $e"; end))
 set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --no-ignore $fd_exclude_args"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set -gx FZF_ALT_C_COMMAND "fd --type d --hidden --follow --no-ignore $fd_exclude_args"
