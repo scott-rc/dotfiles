3ca6acc7652297d55f4785324d5ed7160db00232
Scott Côté <scott.cote@hey.com>
2026-02-03 14:17:52 -0500

    Add --no-ignore to fzf commands with explicit excludes
    Allow fzf to find files in gitignored directories by using --no-ignore,
    while explicitly excluding .git, .DS_Store, and .direnv directories.

--- configs/fish/conf.d/fzf.fish
+++ configs/fish/conf.d/fzf.fish
@@ -2,8 +2,14 @@ if not status is-interactive
     return
 end
 
-set -gx FZF_DEFAULT_COMMAND 'fd --type f --hidden --follow --exclude .git'
+set -l fd_excludes \
+    .git \
+    .DS_Store \
+    .direnv
+
+set -l fd_exclude_args (string join ' ' (for e in $fd_excludes; echo "--exclude $e"; end))
+set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden --follow --no-ignore $fd_exclude_args"
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-set -gx FZF_ALT_C_COMMAND 'fd --type d --hidden --follow --exclude .git'
+set -gx FZF_ALT_C_COMMAND "fd --type d --hidden --follow --no-ignore $fd_exclude_args"
 
 set -gx FZF_DEFAULT_OPTS '--select-1 --height ~40% --reverse'
