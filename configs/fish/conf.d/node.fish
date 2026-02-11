dd2b235e03890a78a215916882227270236c9cdc
Scott Côté <scott.cote@hey.com>
2026-02-11 17:46:07 -0500

    Remove yarn aliases from fish config

--- configs/fish/conf.d/node.fish
+++ configs/fish/conf.d/node.fish
@@ -30,20 +30,4 @@ abbr --add prs pnpm run start
 abbr --add pt pnpm run test
 abbr --add px pnpm x
 
-alias y=yarn
-abbr --add ya yarn add
-abbr --add yad yarn add -D
-abbr --add yb yarn build
-abbr --add yc yarn clean
-abbr --add yd yarn dev
-abbr --add yi yarn install
-abbr --add yl yarn lint
-abbr --add yr yarn run
-abbr --add yrm yarn remove
-abbr --add ys yarn start
-abbr --add yt yarn test
-abbr --add yu yarn upgrade-interactive --latest
-abbr --add yw yarn workspace
-abbr --add yws yarn workspaces
-
 abbr --add vt vitest
