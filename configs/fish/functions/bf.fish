a1851a1a01c1712510809c91c23213890e5a2c97
Scott Côté <scott.cote@hey.com>
2026-02-11 22:00:38 -0500

    Rename fish file-finding functions to consistent naming scheme
    The old names were inconsistent about whether the first letter meant
    the action or the search method (`vf` = vim find, `vr` = view ripgrep,
    `fv` = fuzzy view). Now the first letter is always the output action
    and the second is the search method: `vf`/`vr` open in nvim, `bf`/`br`
    view in bat. This renames `fv` � `bf` and adds `br` for the old `vr`
    behavior (ripgrep � bat), while `vr` now opens matches in nvim.

--- configs/fish/functions/bf.fish
+++ configs/fish/functions/bf.fish
@@ -1,4 +1,4 @@
-function fv --description "Fuzzy find file and view in bat"
+function bf --description "Fuzzy find file and view in bat"
     set -l file (fzf_files --query "$argv")
     or return
     bat $file
--- configs/fish/functions/br.fish
+++ configs/fish/functions/br.fish
@@ -1,7 +1,7 @@
-function vr --description "Fuzzy search file contents with ripgrep, open in editor"
+function br --description "Fuzzy search file contents with ripgrep, view in bat"
     set -l selection (rg --line-number --color=always . | fzf --select-1 --query "$argv" --ansi --delimiter : --preview 'bat --color=always --highlight-line {2} {1}' --preview-window 'up,60%,+{2}-5')
     or return
     set -l file (echo $selection | cut -d: -f1)
     set -l line (echo $selection | cut -d: -f2)
-    nvim "+$line" "$file"
+    bat --highlight-line $line $file
 end
