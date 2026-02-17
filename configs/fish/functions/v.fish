5c57d5bc2abefc46805b82f967f123aee5a4ef10
Scott Côté <scott.cote@hey.com>
2026-02-17 11:44:34 -0500

    Make `v` with no args open current directory instead of fzf

--- configs/fish/functions/v.fish
+++ configs/fish/functions/v.fish
@@ -3,6 +3,8 @@ function v --description "Open file/directory in editor, or fuzzy find"
         nvim $argv[2..]
     else if test (count $argv) -gt 0 -a -e "$argv[1]"
         nvim $argv
+    else if test (count $argv) -eq 0
+        nvim .
     else
         vf $argv
     end
