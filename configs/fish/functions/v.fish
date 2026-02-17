deb4ac1c1ae9885663cda86213e97778e4a174df
Scott Côté <scott.cote@hey.com>
2026-02-16 23:26:21 -0500

    Add `-n` flag to `v` for creating new files

--- configs/fish/functions/v.fish
+++ configs/fish/functions/v.fish
@@ -1,5 +1,7 @@
 function v --description "Open file/directory in editor, or fuzzy find"
-    if test (count $argv) -gt 0 -a -e "$argv[1]"
+    if test (count $argv) -gt 0 -a "$argv[1]" = -n
+        nvim $argv[2..]
+    else if test (count $argv) -gt 0 -a -e "$argv[1]"
         nvim $argv
     else
         vf $argv
