ba4af733e4b19c8c03a11839c2ddf625112f761a
Scott Côté <scott.cote@hey.com>
2026-02-16 20:12:28 -0500

    Show git branch name (without sc/ prefix) in fish title

--- configs/fish/functions/fish_title.fish
+++ configs/fish/functions/fish_title.fish
@@ -1,8 +1,11 @@
 function fish_title
-    # An override for the current command is passed as the first parameter.
     set -l command $argv[1]
     if not set -q command[1]; or test "$command" = fish
-        set command
+        set -l branch (git branch --show-current 2>/dev/null)
+        if test -n "$branch"
+            echo -- (string replace 'sc/' '' -- $branch)
+        end
+        return
     end
     echo -- (string sub -l 20 -- $command)
 end
