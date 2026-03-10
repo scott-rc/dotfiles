3cf83707af2866be91c478dce461e703e418d0a4
Scott Côté <scott.cote@hey.com>
2026-03-10 14:53:01 -0400

    Persist last search independently for grep and file finder

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,40 @@
+local grep_ref = { "" }
+local files_ref = { "" }
+
+local function save_prompt_mappings(ref)
+	local actions = require("telescope.actions")
+	local action_state = require("telescope.actions.state")
+	return function(_, map)
+		local function save(bufnr)
+			ref[1] = action_state.get_current_picker(bufnr):_get_prompt()
+		end
+		map("i", "<Esc>", function(bufnr)
+			save(bufnr)
+			actions.close(bufnr)
+		end)
+		map("i", "<CR>", function(bufnr)
+			save(bufnr)
+			actions.select_default(bufnr)
+		end)
+		return true
+	end
+end
+
+local function live_grep_with_last()
+	require("telescope.builtin").live_grep({
+		default_text = grep_ref[1],
+		attach_mappings = save_prompt_mappings(grep_ref),
+	})
+end
+
+local function find_files_with_last()
+	require("telescope").extensions.frecency.frecency({
+		workspace = "CWD",
+		default_text = files_ref[1],
+		attach_mappings = save_prompt_mappings(files_ref),
+	})
+end
+
 return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = {
@@ -49,30 +86,9 @@ return {
 		telescope.load_extension("frecency")
 	end,
 	keys = {
-		{
-			"<D-f>",
-			function()
-				require("telescope").extensions.frecency.frecency({ workspace = "CWD" })
-			end,
-			mode = { "n", "v", "i" },
-			desc = "Find files",
-		},
-		{
-			"<D-g>",
-			function()
-				require("telescope.builtin").live_grep()
-			end,
-			mode = { "n", "v", "i" },
-			desc = "Ripgrep search",
-		},
-		{
-			"<D-k>",
-			function()
-				require("telescope").extensions.frecency.frecency({ workspace = "CWD" })
-			end,
-			mode = { "n", "v", "i" },
-			desc = "Find files",
-		},
+		{ "<D-f>", find_files_with_last, mode = { "n", "v", "i" }, desc = "Find files (last search)" },
+		{ "<D-g>", live_grep_with_last, mode = { "n", "v", "i" }, desc = "Ripgrep search (last search)" },
+		{ "<D-k>", find_files_with_last, mode = { "n", "v", "i" }, desc = "Find files (last search)" },
 		{
 			"<D-p>",
 			function()
