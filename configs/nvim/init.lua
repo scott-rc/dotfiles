5cacaed212a270d03b12074bb0cbb604eb490343
Scott Côté <scott.cote@hey.com>
2026-04-08 14:45:41 -0400

    ui2

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -2,6 +2,8 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.g.mapleader = " "
 
+require("vim._core.ui2").enable({})
+
 require("config.options")
 require("config.autocmds")
 require("config.keymaps")
