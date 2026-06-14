6bacff4cf02422938f4793f9a0c65a5b4552a5b1
Scott Côté <scott.cote@hey.com>
2026-06-14 08:20:14 -0400

    Tweak fish theme colors for autosuggestion, search, and pager
    Use dimmer 555/B3A06D shades and a bryellow search match for better
    contrast in the frozen color theme.

--- configs/fish/conf.d/fish_frozen_theme.fish
+++ configs/fish/conf.d/fish_frozen_theme.fish
@@ -8,7 +8,7 @@
 # for appropriate commands to add to ~/.config/fish/config.fish instead.
 # See also the release notes for fish 4.3.0 (run `help relnotes`).
 
-set --global fish_color_autosuggestion brblack
+set --global fish_color_autosuggestion 555 brblack
 set --global fish_color_cancel -r
 set --global fish_color_command blue
 set --global fish_color_comment red
@@ -25,13 +25,13 @@ set --global fish_color_operator brcyan
 set --global fish_color_param cyan
 set --global fish_color_quote yellow
 set --global fish_color_redirection cyan --bold
-set --global fish_color_search_match white --background=brblack
+set --global fish_color_search_match bryellow --background=brblack
 set --global fish_color_selection white --bold --background=brblack
 set --global fish_color_status red
 set --global fish_color_user brgreen
 set --global fish_color_valid_path --underline
 set --global fish_pager_color_completion normal
-set --global fish_pager_color_description yellow -i
+set --global fish_pager_color_description B3A06D yellow -i
 set --global fish_pager_color_prefix normal --bold --underline
 set --global fish_pager_color_progress brwhite --background=cyan
 set --global fish_pager_color_selected_background -r
